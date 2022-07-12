3669a72f5f72c0346ac34f77a6266cb779f8da1c
jstojadinovic <jovana.stojadinovic@comtrade.com>
2022-07-12 16:22:50 +0200

    Izmenjena poruka

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to EDIT!</Text>
+      <Text>Welcome to EDIT</Text>
       <StatusBar style="auto" />
     </View>
   );
